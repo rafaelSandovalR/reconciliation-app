9af796f59fe214529f6fc6b3fe01a9fe567bd9c8
Rafael Sandoval <rsandoval0408@gmail.com>
2026-08-11 17:09:34 -0400

    Initial Commit

---
+++ src/main/java/com/apex/reconciliation_app/ReconciliationAppApplication.java
@@ -0,0 +1,13 @@
+package com.apex.reconciliation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconciliationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconciliationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/reconciliation_app/ReconciliationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.reconciliation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconciliationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
